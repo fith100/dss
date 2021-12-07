9a97269a328d0b34e0ec864cfa8346095c353442
murphy <23@gmail.com>
2021-12-07 16:06:49 +0500

    work

---
+++ script.js
@@ -0,0 +1,39 @@
+
+let chill1 = prompt('Введите имя');
+let age1 = +prompt('Введите возраст');
+let chill2 = prompt('Введите имя');
+let age2 = +prompt('Введите возраст');
+let chill3 = prompt('Введите имя');
+let age3 = +prompt('Введите возраст');
+let chill4 = prompt('Введите имя');
+let age4 = +prompt('Введите возраст');
+
+const luck = {
+    'Пользователь 1': {
+        chill: chill1,
+        age3: age1  
+    },
+    'Пользователь 2': {
+        chill: chill2,
+        age3: age2  
+    },
+    'Пользователь 3': {
+        chill: chill3,
+        age3: age3  
+    },
+    'Пользователь 4': {
+        chill: chill4,
+        age3: age4 
+    },
+
+}
+
+
+console.log(' Пользователь 1 ' + ' Имя Пользователя - ' + chill1 + ' Возраст ' + age1);
+console.log(' Пользователь 2 ' + ' Имя Пользователя - ' + chill2 + ' Возраст ' + age2);
+console.log(' Пользователь 3 ' + ' Имя Пользователя - ' + chill3 + ' Возраст ' + age3);
+console.log(' Пользователь 4 ' + ' Имя Пользователя - ' + chill4 + ' Возраст ' + age4);
+
+
+
+
